ee7430e6648a2359b31907845f17e479ccc2b4a0
Shawn Sargent <Bakushawn@gmail.com>
2018-08-04 18:17:47 -0700

    cleaned up RobotMap and added joystick and encoder constants

--- src/RobotMap.h
+++ src/RobotMap.h
@@ -1,25 +1,32 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+#ifndef ROBOTMAP_H
+#define ROBOTMAP_H
 
-#pragma once
+//Joystick Ports
+constexpr int JOY_DRIVER = 0;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+//Joystick Button IDs
+constexpr int BUTTON_A = 1;
+constexpr int BUTTON_B = 2;
+constexpr int BUTTON_X = 3;
+constexpr int BUTTON_Y = 4;
+constexpr int LEFT_BUMP = 5;
+constexpr int RIGHT_BUMP = 6;
+constexpr int BUTTON_SELECT = 7;
+constexpr int BUTTON_START = 8;
 
-// For example to map the left and right motors, you could define the
-// following variables to use with your drivetrain subsystem.
-// constexpr int kLeftMotor = 1;
-// constexpr int kRightMotor = 2;
+//Joystick Axis IDs
+constexpr int LEFT_X = 0;
+constexpr int LEFT_Y = 1;
+constexpr int RIGHT_X = 4;
+constexpr int RIGHT_Y = 5;
+constexpr int LEFT_TRIG = 2;
+constexpr int RIGHT_TRIG = 3;
 
-// If you are using multiple modules, make sure to define both the port
-// number and the module. For example you with a rangefinder:
-// constexpr int kRangeFinderPort = 1;
-// constexpr int kRangeFinderModule = 1;
+//Encoder Channels
+constexpr int LA_CHANNEL = 0;
+constexpr int LB_CHANNEL = 1;
+constexpr int RA_CHANNEL = 2;
+constexpr int RB_CHANNEL = 3;
+
+
+#endif
