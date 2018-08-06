efbc834f4460048f3612e3ae809ffaad534279ae
Shawn Sargent <Bakushawn@gmail.com>
2018-08-05 19:05:37 -0700

    added commandgroups for if we do follow me in autonomous

---
+++ src/CommandGroups/AutoCommand.cpp
@@ -0,0 +1,9 @@
+#include "AutoCommand.h"
+#include <Preferences.h>
+#include <DriverStation.h>
+#include <SmartDashboard/SmartDashboard.h>
+#include <string>
+
+
+AutoCommand::AutoCommand(int pos) {
+}
---
+++ src/CommandGroups/AutoCommand.h
@@ -0,0 +1,13 @@
+#ifndef AutoCommand_H
+#define AutoCommand_H
+
+#include <Commands/CommandGroup.h>
+
+
+class AutoCommand : public CommandGroup {
+public:
+	AutoCommand(int);
+private:
+};
+
+#endif  // AutoCommand_H
